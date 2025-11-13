590ce53c08d4321295d29882c542b95a760f2b73
Carlos del Valle <carlos.delvalle@pyv.systems>
2025-11-13 13:16:00 -0600

    Icono

--- src/UhfPrime.TestBench/MainWindow.axaml.cs
+++ src/UhfPrime.TestBench/MainWindow.axaml.cs
@@ -4,6 +4,9 @@ using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using Avalonia.Input;
 using UhfPrime.TestBench.ViewModels;
+using SkiaSharp;
+using Svg.Skia;
+using System.IO;
 
 namespace UhfPrime.TestBench;
 
@@ -19,12 +22,46 @@ public partial class MainWindow : Window
         DataContext = _viewModel;
         Console.WriteLine($"[Window] DataContext set: {_viewModel?.GetType().Name}");
         _logoImage = this.FindControl<Image>("LogoImage");
+        TrySetWindowIconFromSvg();
     }
 
     protected override async void OnClosed(EventArgs e)
     {
         base.OnClosed(e);
         await _viewModel.DisposeAsync();
+    }
+
+    private void TrySetWindowIconFromSvg()
+    {
+        try
+        {
+            var uri = new Uri("avares://UhfPrime.TestBench/Assets/rfidIcon.svg");
+            using var stream = AssetLoader.Open(uri);
+            // Load SVG and render to a 256x256 bitmap
+            var svg = new SKSvg();
+            svg.Load(stream);
+            var picture = svg.Picture;
+            if (picture is null)
+                return;
+
+            const int size = 256;
+            var rect = picture.CullRect;
+            var scale = Math.Min(size / rect.Width, size / rect.Height);
+            using var surface = SKSurface.Create(new SKImageInfo(size, size, SKColorType.Bgra8888, SKAlphaType.Premul));
+            var canvas = surface.Canvas;
+            canvas.Clear(SKColors.Transparent);
+            var matrix = SKMatrix.CreateScale(scale, scale);
+            canvas.DrawPicture(picture, ref matrix);
+            using var image = surface.Snapshot();
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+            using var ms = new MemoryStream(data.ToArray());
+            var bmp = new Bitmap(ms);
+            this.Icon = new WindowIcon(bmp);
+        }
+        catch
+        {
+            // Ignore failures (e.g., package not present); window will keep default icon
+        }
     }
 
     private void LogoImage_DoubleTapped(object? sender, TappedEventArgs e)
